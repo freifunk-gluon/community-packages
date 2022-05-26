25a2f26b6c2069809793238d54a47e0c7b10a75e
Maciej Krüger <mkg20001@gmail.com>
2022-05-26 12:11:46 +0200

    ffgraz-config-mode-theme-funkfeuer: add package

---
+++ ffgraz-config-mode-theme-funkfeuer/update.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -euo pipefail
+
+gluon="$1"
+
+freifunk='
+@charset "utf-8";
+
+$ffyellow:  #ffb400;
+$ffmagenta: #dc0067;
+$ffzusatz:  #009ee0;
+$red:       #ee3300;
+'
+
+funkfeuer='
+/* Colors adjusted based on the CI from https://download.funkfeuer.at/misc/Funkfeuer-CI/VIguide_FUNKFEUER_last.pdf */
+
+@charset "utf-8";
+
+$feuer_darkblue: #273142;
+$feuer_greyblue: #556270;
+$feuer_lightblue: #6991aa;
+$feuer_red: #f23a14;
+$feuer_green: #a1c820;
+
+$ffyellow:  $feuer_green;
+$ffmagenta: $feuer_lightblue;
+$ffzusatz:  $feuer_darkblue;
+$red:       $feuer_red;
+'
+
+for f in files i18n sass; do
+  rm -rf "$f"
+  cp -vrp "$gluon/package/gluon-config-mode-theme/$f" .
+done
+
+THEME=$(cat sass/gluon.scss)
+THEME_REPLACED=${THEME/"$freifunk"/"$funkfeuer"}
+
+echo "$THEME_REPLACED" > sass/gluon.scss
+
+sass --sourcemap=none -C -t compressed sass/gluon.scss files/lib/gluon/config-mode/www/static/gluon.css
