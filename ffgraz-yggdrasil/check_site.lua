7f7de80e6cb00dafaf2f7316acc829fd6e7906df
Maciej Krüger <mkg20001@gmail.com>
2022-05-30 09:27:38 +0200

    ffgraz-yggdrasil: fix regex

--- ffgraz-yggdrasil/check_site.lua
+++ ffgraz-yggdrasil/check_site.lua
@@ -1,2 +1,2 @@
-need_boolean({'mesh', 'yggdrasil', 'enabled'}, false)
-need_string_array_match({'mesh', 'yggdrasil', 'peers'}, '^[a-z]{3}://.+$', false)
+local required = need_boolean({'mesh', 'yggdrasil', 'enabled'}, false)
+need_string_array_match({'mesh', 'yggdrasil', 'peers'}, '^%l+://.+$', required)
