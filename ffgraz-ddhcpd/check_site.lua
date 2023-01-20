70c47b100fd6f2bf392984a2a6782e4e7dcc6684
Maciej Krüger <mkg20001@gmail.com>
2023-01-20 03:17:40 +0100

    ffgraz-ddhcpd: don't require ddhcpd range if prefix4 is set

--- ffgraz-ddhcpd/check_site.lua
+++ ffgraz-ddhcpd/check_site.lua
@@ -1,7 +1,8 @@
 #!/usr/bin/lua
 
 need_boolean({'ddhcpd', 'enabled'}, false)
-need_string_match({'ddhcpd', 'range'}, '^%d+.%d+.%d+.%d+/%d+$')
+local has_prefix4 = need_string_match(in_domain({'prefix4'}), '^%d+.%d+.%d+.%d+/%d+$', false)
+need_string_match({'ddhcpd', 'range'}, '^%d+.%d+.%d+.%d+/%d+$', not has_prefix4)
 need_number({'ddhcpd', 'block_size'}, false)
 need_number({'ddhcpd', 'block_timeout'}, false)
 need_number({'ddhcpd', 'dhcp_lease_time'}, false)
