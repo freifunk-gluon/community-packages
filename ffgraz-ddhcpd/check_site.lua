53faeabe8b0f46f31a19b58132de7a4e7f06723e
Maciej Krüger <mkg20001@gmail.com>
2023-01-20 03:27:05 +0100

    ffgraz-ddhcpd: require next node ip4

--- ffgraz-ddhcpd/check_site.lua
+++ ffgraz-ddhcpd/check_site.lua
@@ -1,7 +1,9 @@
 #!/usr/bin/lua
 
-need_boolean({'ddhcpd', 'enabled'}, false)
+need_string_match(in_domain({'next_node', 'ip4'}), '^%d+.%d+.%d+.%d+$')
 local has_prefix4 = need_string_match(in_domain({'prefix4'}), '^%d+.%d+.%d+.%d+/%d+$', false)
+
+need_boolean({'ddhcpd', 'enabled'}, false)
 need_string_match({'ddhcpd', 'range'}, '^%d+.%d+.%d+.%d+/%d+$', not has_prefix4)
 need_number({'ddhcpd', 'block_size'}, false)
 need_number({'ddhcpd', 'block_timeout'}, false)
