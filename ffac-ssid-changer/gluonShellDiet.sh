1af59ced0fd6e6549c41cca17e34e1d340cb1889
Grische <2787581+grische@users.noreply.github.com>
2023-11-20 21:40:10 +0100

    ffac-ssid-changer: fix shellcheck issues
    Fixed all issues except SC3057.

--- ffac-ssid-changer/gluonShellDiet.sh
+++ ffac-ssid-changer/gluonShellDiet.sh
@@ -2,4 +2,4 @@
 
 # This script requires a file as argument in which it will remove all comment lines that start with a hash '#'
 
-sed '/^\s*\#[^!].*/d; /^\s*\#$/d' $1
+sed '/^\s*\#[^!].*/d; /^\s*\#$/d' "$1"
