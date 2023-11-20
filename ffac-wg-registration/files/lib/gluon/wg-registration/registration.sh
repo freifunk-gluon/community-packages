7e1e55fbd3d9c6986da2f8418fa1f45633f8d0d6
Grische <2787581+grische@users.noreply.github.com>
2023-11-20 23:03:56 +0100

    fix invalid HTTPS check
    The old code ran wget and checked only if exit code was '1' and then used
    HTTP as a fallback.
    
    wget returns several different exit code:
    https://www.gnu.org/software/wget/manual/html_node/Exit-Status.html
    
    In case for example an "SSL verification failure." occured (expired certs)
    the fallback did not trigger and it tried but failed to establish an HTTPS
    connection.

--- ffac-wg-registration/files/lib/gluon/wg-registration/registration.sh
+++ ffac-wg-registration/files/lib/gluon/wg-registration/registration.sh
@@ -4,11 +4,11 @@ if [ "$(uci get gluon.mesh_vpn.enabled)" == "true" ] || [ "$(uci get gluon.mesh_
         # check if registration has been done since last boot
         if [ ! -f /tmp/WG_REGISTRATION_SUCCESSFUL ]; then
 		# Push public key to broker, test for https and use if supported
-		wget -q https://[::1]
-		if [ $? -eq 1 ]; then
-			PROTO=http
-		else
+		if wget -q "https://[::1]"
+		then
 			PROTO=https
+		else
+			PROTO=http
 		fi
 		PUBLICKEY=$(uci get network.wg_mesh.private_key | wg pubkey)
 		NODENAME=$(uci get system.@system[0].hostname)
