a4b50e3566267e6b7dda357b8f129a2f6791b138
Grische <2787581+grische@users.noreply.github.com>
2023-11-20 22:24:17 +0100

    ffac-wg-registration: fix all shellcheck issues

--- ffac-wg-registration/files/lib/gluon/wg-registration/registration.sh
+++ ffac-wg-registration/files/lib/gluon/wg-registration/registration.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ "$(uci get gluon.mesh_vpn.enabled)" == "true" ] || [ "$(uci get gluon.mesh_vpn.enabled)" == "1" ]; then
-        # check if registration has been done since last boot
-        if [ ! -f /tmp/WG_REGISTRATION_SUCCESSFUL ]; then
+if [ "$(uci get gluon.mesh_vpn.enabled)" = "true" ] || [ "$(uci get gluon.mesh_vpn.enabled)" = "1" ]; then
+	# check if registration has been done since last boot
+	if [ ! -f /tmp/WG_REGISTRATION_SUCCESSFUL ]; then
 		# Push public key to broker, test for https and use if supported
-		wget -q https://[::1]
+		wget -q "https://[::1]"
 		if [ $? -eq 1 ]; then
 			PROTO=http
 		else
@@ -14,8 +14,8 @@ if [ "$(uci get gluon.mesh_vpn.enabled)" == "true" ] || [ "$(uci get gluon.mesh_
 		NODENAME=$(uci get system.@system[0].hostname)
 		BROKER=$(uci get wireguard.mesh_vpn.broker)
 		logger -t wg-registration "Post $NODENAME and $PUBLICKEY to $PROTO://$BROKER"
-		gluon-wan wget -q  -O- --post-data='{"node_name": "'"$NODENAME"'","public_key": "'"$PUBLICKEY"'"}' $PROTO://$BROKER
-		if [ $? -eq 0 ]; then
+		if gluon-wan wget -q  -O- --post-data='{"node_name": "'"$NODENAME"'","public_key": "'"$PUBLICKEY"'"}' $PROTO://"$BROKER"
+		then
 			touch /tmp/WG_REGISTRATION_SUCCESSFUL
 			logger -t wg-registration "successfully registered wg publickey"
 		fi
