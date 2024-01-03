0f9f758e0927dcd344800dbc9084465e91670b6f
Florian Maurer <f.maurer@outlook.de>
2024-01-03 19:54:20 +0100

    ff-ap-timer: specify variables as local
    pass luacheck
    rename translation file

--- ff-web-ap-timer/luasrc/lib/gluon/config-mode/model/admin/ap-timer.lua
+++ ff-web-ap-timer/luasrc/lib/gluon/config-mode/model/admin/ap-timer.lua
@@ -1,6 +1,6 @@
 local uci = require('simple-uci').cursor()
 
-pkg_i18n = i18n 'ff-web-ap-timer'
+local pkg_i18n = i18n 'ff-web-ap-timer'
 
 if not uci:get('ap-timer', 'settings') then
 	uci:section('ap-timer', 'ap-timer', 'settings')
@@ -15,16 +15,16 @@ end
 local f = Form(pkg_i18n.translate('AP Timer'), pkg_i18n.translate(
 	'You can setup the AP Timer here'))
 
-local s = f:section(Section)
+local sec1 = f:section(Section)
 
-enabled = s:option(Flag, 'enabled', pkg_i18n.translate('Enabled'))
+local enabled = sec1:option(Flag, 'enabled', pkg_i18n.translate('Enabled'))
 enabled.default = uci:get_bool('ap-timer', 'settings', 'enabled')
 enabled.optional = false
 function enabled:write(data)
 	uci:set('ap-timer', 'settings', 'enabled', data)
 end
 
-local timer_type = s:option(ListValue, 'type', pkg_i18n.translate('Type'))
+local timer_type = sec1:option(ListValue, 'type', pkg_i18n.translate('Type'))
 timer_type.default = uci:get('ap-timer', 'settings', 'type')
 timer_type:depends(enabled, true)
 timer_type:value('day', pkg_i18n.translate('Daily'))
@@ -32,25 +32,25 @@ function timer_type:write(data)
 	uci:set('ap-timer', 'settings', 'type', data)
 end
 
-local s = f:section(Section)
+local sec2 = f:section(Section)
 
-o = s:option(DynamicList, 'on', pkg_i18n.translate('ON'))
-o.default = uci:get_list('ap-timer', 'all', 'on')
-o.placeholder = '06:30'
-o:depends(timer_type, 'day')
-o.optional = false
-o.datatype = 'minlength(5)'
-function o:write(data)
+local on = sec2:option(DynamicList, 'on', pkg_i18n.translate('ON'))
+on.default = uci:get_list('ap-timer', 'all', 'on')
+on.placeholder = '06:30'
+on:depends(timer_type, 'day')
+on.optional = false
+on.datatype = 'minlength(5)'
+function on:write(data)
 	uci:set_list('ap-timer', 'all', 'on', data)
 end
 
-o = s:option(DynamicList, 'off', pkg_i18n.translate('OFF'))
-o.default = uci:get_list('ap-timer', 'all', 'off')
-o.placeholder = '23:00'
-o:depends(timer_type, 'day')
-o.optional = false
-o.datatype = 'minlength(5)'
-function o:write(data)
+local off = sec2:option(DynamicList, 'off', pkg_i18n.translate('OFF'))
+off.default = uci:get_list('ap-timer', 'all', 'off')
+off.placeholder = '23:00'
+off:depends(timer_type, 'day')
+off.optional = false
+off.datatype = 'minlength(5)'
+function off:write(data)
 	uci:set_list('ap-timer', 'all', 'off', data)
 end
 
