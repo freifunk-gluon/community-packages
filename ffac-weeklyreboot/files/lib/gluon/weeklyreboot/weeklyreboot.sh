5cb8773aed82aed5b08dbd8e45b165fcd7f07733
Bob Idle <102661087+bobidle@users.noreply.github.com>
2024-02-17 01:09:23 +0100

    ffac-weeklyreboot: fix typos

--- ffac-weeklyreboot/files/lib/gluon/weeklyreboot/weeklyreboot.sh
+++ ffac-weeklyreboot/files/lib/gluon/weeklyreboot/weeklyreboot.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 DELAYTIME=$(</dev/urandom sed 's/[^[:digit:]]\+//g' | head -c4)
-logger -s -t "ffac-weeklyreboot" -p 5 "sheduled reboot in $DELAYTIME seconds"
+logger -s -t "ffac-weeklyreboot" -p 5 "scheduled reboot in $DELAYTIME seconds"
 sleep "$DELAYTIME"
-logger -s -t "ffac-weeklyreboot" -p 5 "sheduled reboot in 5 seconds"
+logger -s -t "ffac-weeklyreboot" -p 5 "scheduled reboot in 5 seconds"
 sleep 5
 # Autoupdate?
 upgrade_started='/tmp/autoupdate.lock'
 if [ -f $upgrade_started ] ; then
-  logger -s -t "ffac-weeklyreboot" -p 5 "Autoupdate läuft! Aborting"
+  logger -s -t "ffac-weeklyreboot" -p 5 "Autoupdate running! Aborting"
   exit 2
  fi
 reboot
