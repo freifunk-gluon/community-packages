7e5fa4a2c260b3b1e8dc0ea04b23e43890dd50f3
Tobias <tobias-m99@gmx.de>
2024-03-26 20:03:29 +0000

    fix fallback error

--- ffmuc-mesh-vpn-wireguard-vxlan/luasrc/lib/gluon/gluon-mesh-wireguard-vxlan/parse-wgkex-response.lua
+++ ffmuc-mesh-vpn-wireguard-vxlan/luasrc/lib/gluon/gluon-mesh-wireguard-vxlan/parse-wgkex-response.lua
@@ -2,10 +2,18 @@ local json = require 'jsonc'
 
 local input = assert(arg[1])
 local data = assert(json.parse(input))
+
+-- v1
+if data.Message == "OK" then
+        return
+end
+
+-- v2
 if not data.Endpoint or not data.Endpoint.Address or not data.Endpoint.Port
     or not data.Endpoint.PublicKey or not data.Endpoint.AllowedIPs or not data.Endpoint.AllowedIPs[1] then
     error("Malformed JSON response, missing required value")
 end
+
 print(data.Endpoint.Address)
 print(data.Endpoint.Port)
 print(data.Endpoint.PublicKey)
