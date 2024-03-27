45928cd78e5a18f17da4bbe00ae6c92825556095
Tobias <tobias-m99@gmx.de>
2024-03-27 14:37:25 +0100

    add better error messages

--- ffmuc-mesh-vpn-wireguard-vxlan/luasrc/lib/gluon/gluon-mesh-wireguard-vxlan/parse-wgkex-response.lua
+++ ffmuc-mesh-vpn-wireguard-vxlan/luasrc/lib/gluon/gluon-mesh-wireguard-vxlan/parse-wgkex-response.lua
@@ -9,10 +9,12 @@ if data.Message == "OK" then
 end
 
 -- v2
+assert(data.Endpoint, "Malformed JSON response, missing required value: Endpoint")
 local address = assert(data.Endpoint.Address, "Malformed JSON response, missing required value: Address")
 local port = assert(data.Endpoint.Port, "Malformed JSON response, missing required value: Port")
 local publicKey = assert(data.Endpoint.PublicKey, "Malformed JSON response, missing required value: PublicKey")
-local allowedIPs1 = assert(data.Endpoint.AllowedIPs[1], "Malformed JSON response, missing required value: AllowedIPs")
+assert(data.Endpoint.AllowedIPs, "Malformed JSON response, missing required value: AllowedIPs")
+local allowedIPs1 = assert(data.Endpoint.AllowedIPs[1], "Malformed JSON response, missing required value: AllowedIPs1")
 
 print(address)
 print(port)
