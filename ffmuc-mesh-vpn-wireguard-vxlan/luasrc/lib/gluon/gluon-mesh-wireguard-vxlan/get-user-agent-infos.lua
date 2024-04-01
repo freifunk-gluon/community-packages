5080713b21845d3f4f6345461b44323437118094
Tobias <admin+github@t0biii.de>
2024-04-01 11:32:47 +0200

    Update Formating

--- ffmuc-mesh-vpn-wireguard-vxlan/luasrc/lib/gluon/gluon-mesh-wireguard-vxlan/get-user-agent-infos.lua
+++ ffmuc-mesh-vpn-wireguard-vxlan/luasrc/lib/gluon/gluon-mesh-wireguard-vxlan/get-user-agent-infos.lua
@@ -7,9 +7,9 @@ local data = assert(json.parse(util.exec('ubus call system board')), "Malformed 
 
 assert(data.release, "Malformed JSON response, missing required value: release")
 local Openwrt_version = assert(data.release.version, "Malformed JSON response, missing required value: version")
+local target = assert(data.release.target, "Malformed JSON response, missing required value: target")
+local board_name = assert(data.board_name, "Malformed JSON response, missing required value: board_name")
 local kernel = assert(data.kernel, "Malformed JSON response, missing required value: kernel")
-
-local model = assert(data.model, "Malformed JSON response, missing required value: model")
 
 -- Get Gluon Infos
 local infos = info.get_info()
@@ -17,8 +17,9 @@ local infos = info.get_info()
 local gluon_version = assert(infos.gluon_version, "Malformed gluon-info, missing required value: gluon version")
 local firmware_release = assert(infos.firmware_release, "Malformed gluon-info, missing required value: firmware release")
 
+print(gluon_version)
+print(board_name)
 print(Openwrt_version)
 print(kernel)
-print(gluon_version)
+print(target)
 print(firmware_release)
-print(model)
