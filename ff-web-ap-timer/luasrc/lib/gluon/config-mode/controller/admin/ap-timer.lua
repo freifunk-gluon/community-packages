6eb1cc9e27f3c7bfda438171263a2dcc77f49e8e
Florian Maurer <f.maurer@outlook.de>
2025-09-05 15:04:30 +0200

    ff-web-ap-timer: do not show config-mode settings if no wifi-iface available
    (cherry picked from commit 1ec1cb616518b69c1e8f90137478d8c0f3c34485)

--- ff-web-ap-timer/luasrc/lib/gluon/config-mode/controller/admin/ap-timer.lua
+++ ff-web-ap-timer/luasrc/lib/gluon/config-mode/controller/admin/ap-timer.lua
@@ -1,2 +1,8 @@
+local uci = require("simple-uci").cursor()
+local wireless = require 'gluon.wireless'
+
 package 'ff-web-ap-timer'
-entry({"admin", "ap-timer"}, model("admin/ap-timer"), _("AP Timer"), 30)
+
+if wireless.device_uses_wlan(uci) then
+	entry({"admin", "ap-timer"}, model("admin/ap-timer"), _("AP Timer"), 30)
+end
